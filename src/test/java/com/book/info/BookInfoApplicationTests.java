00e11a9ea7763346c4af309f8b8237df1d06cd40
Sujeet <sujeetbarauli5@gmail.com>
2024-08-27 18:23:28 +0530

    first commit

---
+++ src/test/java/com/book/info/BookInfoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.book.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class BookInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
